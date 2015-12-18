7b59499e874ca645be50532b3662b28d3539bd03
Manuel Ceron <ceronman@gmail.com>
2015-12-17 19:06:21 -0500

    Added solution for day 5 part 2.

---
+++ day_5/nice_words_part2.py
@@ -0,0 +1,21 @@
+nice = 0
+
+# Don't use regexes, i'd be fun they said...
+
+for word in open('input.txt'):
+    pairs_pos = {}
+    double_pair = 0
+    repeats = 0
+    for i in range(len(word)):
+        if i < len(word)-2 and word[i] == word[i+2]:
+            repeats += 1
+
+        pair = word[i:i+2]
+        if len(pair) == 2 and pairs_pos.get(pair, i) != i:
+            double_pair += 1
+        pairs_pos[pair] = i + 1
+
+    if double_pair >= 1 and repeats >= 1:
+        nice += 1
+
+print(nice)
