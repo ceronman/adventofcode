f3864d1f1096947c1309f68d8d48d8dc12c369d4
Manuel Ceron <ceronman@gmail.com>
2015-12-17 21:04:24 -0500

    Added solution to day 6

---
+++ day_6/lights.py
@@ -0,0 +1,17 @@
+grid = [[0] * 1000 for _ in range(1000)]
+
+for line in open('input.txt'):
+    command, start, _, end = line.rsplit(' ', 3)
+    start_x, start_y = [int(c) for c in start.split(',')]
+    end_x, end_y = [int(c) for c in end.split(',')]
+
+    for y in range(start_y, end_y+1):
+        for x in range(start_x, end_x+1):
+            if command == 'turn on':
+                grid[y][x] = 1
+            elif command == 'turn off':
+                grid[y][x] = 0
+            elif command == 'toggle':
+                grid[y][x] = 0 if grid[y][x] else 1
+
+print(sum(sum(row) for row in grid))
