84dcf59360ecdb473205abb31f87c64e09a97173
Manuel Ceron <ceronman@gmail.com>
2015-12-19 17:47:38 -0500

    Added solution for day 9 part 1 and 2

---
+++ day_9/routes_part2.py
@@ -0,0 +1,14 @@
+import re
+import itertools
+
+distances = {}
+places = set()
+
+for line in open('input.txt'):
+    a, b, distance = re.match(r'(\w+) to (\w+) = (\d+)', line).groups()
+    distances[(a, b)] = int(distance)
+    distances[(b, a)] = int(distance)
+    places.update((a, b))
+
+print(max(sum(distances[(route[i], route[i+1])] for i in range(len(route)-1))
+          for route in itertools.permutations(places)))
