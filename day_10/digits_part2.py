ed2ec2c6a7bd63464b3983825d93789c5a66a284
Manuel Ceron <ceronman@gmail.com>
2015-12-19 18:56:42 -0500

    Added solution for day 10 part 1 and 2

---
+++ day_10/digits_part2.py
@@ -0,0 +1,9 @@
+from itertools import groupby
+
+digits = '1113222113'
+repeat = 50
+
+for _ in range(repeat):
+    digits = ''.join(str(len(list(v))) + k for k, v in groupby(digits))
+
+print(len(digits))
