99dac421705efa70b531dde45a46edd8475002e6
Manuel Ceron <ceronman@gmail.com>
2015-12-21 17:55:05 -0500

    Added solutions for day 13 parts 1 and 2

---
+++ day_13/happiness.py
@@ -0,0 +1,22 @@
+import re
+import itertools
+
+
+people = set()
+point_diff = {}
+for line in open('input.txt'):
+    m = re.match(r'(\w+) would (gain|lose) (\d+) happiness '
+                 r'units by sitting next to (\w+)', line)
+    person1, sign, points, person2 = m.groups()
+    sign = '-' if sign == 'lose' else ''
+    point_diff.setdefault(person1, {})[person2] = int(sign + points)
+    people.update((person1, person2))
+
+totals = []
+for comb in itertools.permutations(people):
+    total = 0
+    for i, person in enumerate(comb):
+        total += point_diff[person][comb[i-1]]
+        total += point_diff[person][comb[i+1 if i < len(comb)-1 else 0]]
+    totals.append(total)
+print(max(totals))
