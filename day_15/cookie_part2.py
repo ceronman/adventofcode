78ffb2391882169b6d7c17177a5ad24788b9c09a
Manuel Ceron <ceronman@gmail.com>
2015-12-22 16:43:06 -0500

    Added solutions for day 15 parts 1 and 2

---
+++ day_15/cookie_part2.py
@@ -0,0 +1,25 @@
+from itertools import product
+from functools import reduce
+import re
+
+
+properties = {}
+for line in open('input.txt'):
+    m = re.match(
+        r'(\w+): capacity ([\-\d]+), durability ([\-\d]+), flavor ([\-\d]+), '
+        r'texture ([\-\d]+), calories ([\-\d]+)', line)
+    ingredient, *props = m.groups()
+    properties[ingredient] = [int(p) for p in props]
+
+combinations = [c for c in product(range(100+1), repeat=len(properties))
+                if sum(c) == 100]
+total_scores = []
+for combination in combinations:
+    score = [0] * 5
+    for teaspons, ingredient in zip(combination, sorted(properties)):
+        for i, prop in enumerate(properties[ingredient]):
+            score[i] += teaspons * prop
+    if (score[4] == 500):
+        total_scores.append(reduce(lambda a, b: max(a, 0) * max(b, 0), score[:4]))
+
+print(max(total_scores))
