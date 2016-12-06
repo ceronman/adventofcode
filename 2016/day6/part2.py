c67da3862a0e49b1692bf420f40ccf434181abc2
Manuel Ceron <ceronman@gmail.com>
2016-12-06 20:43:13 +0100

    Added input for day6

---
+++ 2016/day6/part2.py
@@ -0,0 +1,30 @@
+from collections import Counter
+
+# input_lines = '''\
+# eedadn
+# drvtee
+# eandsr
+# raavrd
+# atevrs
+# tsrnev
+# sdttsa
+# rasrtv
+# nssdts
+# ntnada
+# svetve
+# tesnvt
+# vntsnd
+# vrdear
+# dvrsen
+# enarar'''.splitlines()
+
+input_lines = open('input.txt')
+
+counters = []
+for line in input_lines:
+    for i, char in enumerate(line):
+        if i >= len(counters):
+            counters.append(Counter())
+        counters[i].update([char])
+
+print(''.join(c.most_common()[-1][0] for c in counters))
