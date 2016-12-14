7a9d2c4f6298c6494a05722abad926f48874fe08
Manuel Ceron <ceronman@gmail.com>
2016-12-14 14:05:26 +0100

    Added solution for day 14

---
+++ 2016/day14/part1.py
@@ -0,0 +1,38 @@
+import re
+from hashlib import md5
+
+# salt = 'abc'
+salt = 'jlmsuwbz'
+
+repeated3 = re.compile(r'([0-9a-f])\1\1')
+repeated5 = re.compile(r'([0-9a-f])\1\1\1\1')
+
+def genhash(i):
+    base = (salt + str(i)).encode('ascii')
+    h = md5()
+    h.update(base)
+    return h.hexdigest()
+
+i = 0
+keys = []
+hashes = []
+while len(keys) < 64:
+    h = genhash(i)
+    found = {}
+    match = repeated3.search(h)
+    if match:
+        char, = match.groups()
+        found[char] = found.get(char, 0) + 1
+
+    for char in repeated5.findall(h):
+        found[char] = found.get(char, 0) + 1
+    hashes.append(found)
+    if len(hashes) > 1000:
+        for char in hashes.pop(0).keys():
+            for f in hashes:
+                if f.get(char, 0) == 2:
+                    keys.append(i - 1000)
+                    break
+    i+=1
+
+print(keys[-1])
