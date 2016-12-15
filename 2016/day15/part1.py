a1290bb66d1a5b631ad65b3bf868a5b5215de94f
Manuel Ceron <ceronman@gmail.com>
2016-12-15 17:02:48 +0100

    Added solution for day 15

---
+++ 2016/day15/part1.py
@@ -0,0 +1,27 @@
+import re
+
+input_lines = '''\
+Disc #1 has 5 positions; at time=0, it is at position 4.
+Disc #2 has 2 positions; at time=0, it is at position 1.'''.splitlines()
+
+input_lines = open('input.txt')
+
+input_re = re.compile(r'Disc #\d+ has (\d+) positions; at time=0, it is at position (\d+)')
+
+discs = []
+for line in input_lines:
+    match = input_re.match(line)
+    if match:
+        discs.append(list(map(int, match.groups())))
+
+i=0
+done = False
+while not done:
+    for n, (size, pos) in enumerate(discs, 1):
+        if (pos + i + n) % size != 0:
+            i += 1
+            break
+    else:
+        done = True
+print(i)
+
