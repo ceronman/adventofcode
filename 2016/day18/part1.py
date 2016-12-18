00386ae62168d8d94d56063b279ca8384a042d77
Manuel Ceron <ceronman@gmail.com>
2016-12-18 16:11:08 +0100

    Added solution for day 18

---
+++ 2016/day18/part1.py
@@ -0,0 +1,21 @@
+# first_row = '.^^.^.^^^^'
+first_row = open('input.txt').read()
+
+def next_row(row):
+    new = ''
+    for i in range(len(row)):
+        l = row[i-1] if i > 0 else '.'
+        c = row[i]
+        r = row[i + 1] if i < len(row)-1 else '.'
+        prev = l + c + r
+        if prev in ('^^.', '.^^', '^..', '..^'):
+            new += '^'
+        else:
+            new += '.'
+    return new
+
+rows = [first_row]
+for i in range(1, 40):
+    rows.append(next_row(rows[i-1]))
+
+print(sum(sum(1 for c in row if c == '.') for row in rows))
