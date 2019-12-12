687672f23c3bcb01b892c2d3bd21cd62360d38e5
Manuel Ceron <manuel.ceron@booking.com>
2019-12-12 13:07:59 +0100

    Added day 11

---
+++ 2019/day11/part2.clj
@@ -0,0 +1,138 @@
+(defn create-computer [program input]
+  (let [instructions (map read-string (clojure.string/split program #","))]
+    {:pc 0
+     :relative-base 0
+     :memory (into {} (map-indexed vector (map bigint instructions)))
+     :output []
+     :input input
+     :halted false}))
+
+(defn read-word [computer address]
+  (get-in computer [:memory address] 0))
+
+(defn store-word [computer address value]
+  (assoc-in computer [:memory address] (bigint value)))
+
+(defn read-word-relative [computer offset]
+  (read-word computer (+ (:pc computer) offset)))
+
+(defn parse-opcode [computer]
+  (let [opcode (read-word-relative computer 0)
+        instruction (mod opcode 100)
+        modes (quot opcode 100)]
+    {:instruction instruction
+     :modes modes}))
+
+(defn parameter-mode [modes pos]
+  (int (mod (quot modes (Math/pow 10 (dec pos))) 10)))
+
+(defn parameter-address [computer position]
+  (case (parameter-mode (:modes (parse-opcode computer)) position)
+    0 (read-word-relative computer position)
+    2 (+ (:relative-base computer) (read-word-relative computer position))
+    (throw (Exception. "Unknown parameter address"))))
+
+(defn parameter-value [computer position]
+  (case (parameter-mode (:modes (parse-opcode computer)) position)
+    0 (read-word computer (parameter-address computer position))
+    1 (read-word-relative computer position)
+    2 (read-word computer (parameter-address computer position))
+    (throw (Exception. "Unknown parameter mode"))))
+
+(defn advance [computer size]
+  (update computer :pc (partial + size)))
+
+(defn binary-op [computer f]
+  (let [value1 (parameter-value computer 1)
+        value2 (parameter-value computer 2)
+        result (f value1 value2)
+        dest (parameter-address computer 3)]
+    (-> computer
+        (store-word dest result)
+        (advance 4))))
+
+(defn input-op [computer]
+  (let [input-value (first (:input computer))]
+    (-> (update computer :input rest)
+        (store-word (parameter-address computer 1) (bigint input-value))
+        (advance 2))))
+
+(defn output-op [computer]
+  (-> computer
+      (update :output conj (parameter-value computer 1))
+      (advance 2)))
+
+(defn adjust-relative-base-op [computer]
+  (-> computer
+      (update :relative-base + (parameter-value computer 1))
+      (advance 2)))
+
+(defn jump-op [computer pred]
+  (if (pred (parameter-value computer 1))
+    (assoc computer :pc (parameter-value computer 2))
+    (advance computer 3)))
+
+(defn run [computer input]
+  (loop [computer (assoc computer :input input :output [])]
+    (let [opcode (parse-opcode computer)]
+      (case (:instruction opcode)
+        1 (recur (binary-op computer +))
+        2 (recur (binary-op computer *))
+        3 (if (empty? (:input computer))
+            computer ; yield to scheduler
+            (recur (input-op computer)))
+        4 (recur (output-op computer))
+        5 (recur (jump-op computer (complement zero?)))
+        6 (recur (jump-op computer zero?))
+        7 (recur (binary-op computer #(if (< %1 %2) (bigint 1) (bigint 0))))
+        8 (recur (binary-op computer #(if (= %1 %2) (bigint 1) (bigint 0))))
+        9 (recur (adjust-relative-base-op computer))
+        99 (assoc computer :halted true)
+        (throw (Exception. "Unknown instruction"))))))
+
+(defn add-input [computer input]
+  (update computer :input conj input))
+
+(defn rotate [robot rotation]
+  (case rotation
+    0 (update robot :direction #(mod (dec %) 4))
+    1 (update robot :direction #(mod (inc %) 4))
+    (throw (Exception. "Unknown rotation value"))))
+
+(defn move-forward [robot]
+  (let [delta (nth [[0 -1] [1 0] [0 1] [-1 0]] (:direction robot))]
+    (update robot :pos #(map + % delta))))
+
+(defn paint [robot color]
+  (assoc-in robot [:paint (:pos robot)] color))
+
+(defn current-color [robot]
+  (get (:paint robot) (:pos robot) 0))
+
+
+(defn run-robot []
+  (loop [computer (create-computer (slurp "input.txt") [])
+         robot {:pos [0 0] :direction 0 :paint  {[0 0] 1}}]
+    (if (:halted computer)
+      (:paint robot)
+      (let [execution (run computer [(current-color robot)])
+            [new-color rotation] (:output execution)]
+        (recur execution
+               (-> robot
+                   (paint new-color)
+                   (rotate rotation)
+                   (move-forward)))))))
+
+(defn render-paint [paint]
+  (let [min-x (apply min (map first (keys paint)))
+        max-x (apply max (map first (keys paint)))
+        min-y (apply min (map second (keys paint)))
+        max-y (apply max (map second (keys paint)))
+        lines (map (fn [y] 
+                     (clojure.string/join (map (fn [x] 
+                                                 (if (= 1 (paint [x y])) "#" " "))
+                                               (range min-x (inc max-x))))) 
+                   (range min-y (inc max-y)))]
+    (map println lines)))
+
+(render-paint (run-robot))
