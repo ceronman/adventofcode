96960e3cd37102c071c82e9d59524709f263b2a9
Manuel Ceron <manuel.ceron@jetbrains.com>
2021-03-03 16:39:21 +0100

    Added solution for day 2 part 1

---
+++ 2020/adventofcode2020/src/day2.rs
@@ -0,0 +1,53 @@
+use std::fs;
+
+struct Entry {
+    min: usize,
+    max: usize,
+    c: char,
+    password: String
+}
+
+impl Entry {
+    fn parse(line: &str) -> Entry {
+        let parts = line.split(": ").collect::<Vec<&str>>();
+        let policy = parts[0];
+        let password = String::from(parts[1]);
+
+        let parts = policy.split(" ").collect::<Vec<&str>>();
+        let ranges = parts[0];
+        let c = parts[1].chars().next().unwrap();
+
+        let parts = ranges.split("-").collect::<Vec<&str>>();
+        let min = parts[0].parse::<usize>().unwrap();
+        let max = parts[1].parse::<usize>().unwrap();
+
+        Entry {
+            min,
+            max,
+            c,
+            password
+        }
+    }
+
+    fn validate(&self) -> bool {
+        let count = self.password.chars().filter(|c| *c == self.c).count();
+        count >= self.min && count <=self.max
+    }
+}
+
+fn parse_input(input: &str) -> Vec<Entry> {
+    input.lines().map(Entry::parse).collect()
+}
+
+pub fn part1() -> usize {
+    // let input = "1-3 a: abcde\n\
+    //              1-3 b: cdefg\n\
+    //              2-9 c: ccccccccc";
+    let input = fs::read_to_string("data/day2/input.txt")
+        .expect("Unable to read file");
+    
+    parse_input(input.as_str())
+        .into_iter()
+        .filter(|x| x.validate())
+        .count()
+}
--- 2020/adventofcode2020/src/main.rs
+++ 2020/adventofcode2020/src/main.rs
@@ -1,7 +1,9 @@
 mod day1;
+mod day2;
 
 
 fn main() {
     println!("Day 1 Part 1 {}", day1::part1());
     println!("Day 1 Part 2 {}", day1::part2());
+    println!("Day 2 Part 1 {}", day2::part1());
 }
